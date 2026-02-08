c7dbb2e16637c6aacd4a083cafeef1a338acdd17
Muhammad Ashhad <146670025+3Ddaduu@users.noreply.github.com>
2026-02-08 19:48:26 +0500

    Update theme.ts

--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -1,13 +1,12 @@
 import { hopeTheme } from 'vuepress-theme-hope';
-
 import { enNavbarConfig, zhNavbarConfig } from './navbar.js';
 import { enSidebarConfig, zhSidebarConfig } from './sidebar.js';
 
 export default hopeTheme(
   {
+    // --- 1. Basic Settings ---
     hostname: 'https://waline.js.org',
     favicon: '/favicon.ico',
-
     logo: '/logo.png',
     repo: 'walinejs/waline',
     docsDir: 'docs/src',
@@ -16,24 +15,23 @@ export default hopeTheme(
     pageInfo: ['ReadingTime', 'PageView'],
     contributors: false,
 
+    // --- 2. Localization Settings ---
     locales: {
       '/': {
         navbar: zhNavbarConfig,
         sidebar: zhSidebarConfig,
-
         footer: 'GPL-2.0 协议 | Copyright © 2020-present lizheming',
         displayFooter: true,
       },
-
       '/en/': {
         navbar: enNavbarConfig,
         sidebar: enSidebarConfig,
-
         footer: 'GPL-2.0 LICENSE | Copyright © 2020-present lizheming',
         displayFooter: true,
       },
     },
 
+    // --- 3. Markdown Features ---
     markdown: {
       codeTabs: true,
       figure: true,
@@ -45,47 +43,49 @@ export default hopeTheme(
       tasklist: true,
     },
 
-   comment: {
-  provider: 'Waline',
-  serverURL: "https://walinecmnt-docs.vercel.app", // MAKE SURE THIS IS YOUR URL
-  tex: false,      // FIX: This stops the "@mathjax/src" error
-  pageview: false, // FIX: This stops the "Rollup failed to resolve" error
-  locales: {
-    '/': {
-      admin: '可爱的管理员',
-      level0: '锻体',
-      level1: '炼气',
-      level2: '筑基',
-      level3: '金丹',
-      level4: '元婴',
-      level5: '化神',
+    // --- 4. Comment Settings (Waline) ---
+    comment: {
+      provider: 'Waline',
+      serverURL: "https://walinecmnt-docs.vercel.app", // Use your URL here
+      tex: false,      // Disables MathJax in comments to avoid errors
+      pageview: false, // Disables pageview counts to avoid Rollup errors
+      locales: {
+        '/': {
+          admin: '可爱的管理员',
+          level0: '锻体',
+          level1: '炼气',
+          level2: '筑基',
+          level3: '金丹',
+          level4: '元婴',
+          level5: '化神',
+        },
+        '/en/': {
+          admin: 'GM',
+          level0: 'Dwarves',
+          level1: 'Hobbits',
+          level2: 'Ents',
+          level3: 'Wizards',
+          level4: 'Elves',
+          level5: 'Maiar',
+        },
+      },
     },
-    '/en/': {
-      admin: 'GM',
-      level0: 'Dwarves',
-      level1: 'Hobbits',
-      level2: 'Ents',
-      level3: 'Wizards',
-      level4: 'Elves',
-      level5: 'Maiar',
+
+    // --- 5. Additional Components & Icons ---
+    components: {
+      components: ['BiliBili', 'VidStack'],
+    },
+
+    docsearch: {
+      appId: 'W34KABV4KM',
+      apiKey: 'd189586c601d439f9247bdaf95b3555f',
+      indexName: 'waline',
+    },
+
+    icon: {
+      assets: '//at.alicdn.com/t/c/font_3180165_6o97v6oir2j.css',
     },
   },
-},
-
-      components: {
-        components: ['BiliBili', 'VidStack'],
-      },
-
-      docsearch: {
-        appId: 'W34KABV4KM',
-        apiKey: 'd189586c601d439f9247bdaf95b3555f',
-        indexName: 'waline',
-      },
-
-      icon: {
-        assets: '//at.alicdn.com/t/c/font_3180165_6o97v6oir2j.css',
-      },
-    },
-  },
-  { custom: true },
+  // The second argument for hopeTheme handles behavior options like 'custom'
+  { custom: true }
 );
